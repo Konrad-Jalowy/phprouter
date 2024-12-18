a5d3367a6b3cf431367e82e20c1bcfcca96807a5
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-18 11:20:11 +0100

    autoloading works

--- public/index.php
+++ public/index.php
@@ -1,6 +1,17 @@
 <?php
 require '../helpers.php';
-requireMe();
+
+spl_autoload_register(function($class){
+    $path = basePath("Framework/$class.php");
+    if(file_exists($path)){
+        require_once $path;
+    }
+});
+
+$router = new Router();
+$session = new Session();
+var_dump($router);
+var_dump($session);
 echo "</br>";
 echo "hello world</br>";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
