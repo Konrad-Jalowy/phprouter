cb241718709c0a5c7828e3358b03d45a12f28edd
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-18 16:28:26 +0100

    active users count added

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -2,13 +2,17 @@
 class HomeController {
 
     public function index(){
+        Session::onEveryRequestMiddleware();
         loadView('index');
     }
     public function single($id){
+        Session::onEveryRequestMiddleware();
         $person = (object)["name" => "Jane Doe $id", "age" => (18+$id), "gender" => "Female"];
         loadView('single', compact('person'));
     }
     public function active(){
-        loadView('active', ["number" => 42]);
+        Session::onEveryRequestMiddleware();
+        $number = Session::sessions5Minutes();
+        loadView('active', compact("number"));
     }
 };
