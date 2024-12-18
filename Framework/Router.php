261152ac2aff18746abf5ae64565c0dcdcc5b00f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-18 11:16:47 +0100

    were getting somewhere

--- Framework/Router.php
+++ Framework/Router.php
@@ -4,6 +4,10 @@ class Router
 {
   protected $routes = [];
   protected $prefix = "";
-
+  
+  public function route($uri){
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    echo "$requestMethod -> $uri </br>";
+  }
 
 }
