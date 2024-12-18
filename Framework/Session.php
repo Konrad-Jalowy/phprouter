6ceb2cd6b3b6f063952168f7c2c0f219d2d1326f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-18 14:38:13 +0100

    counting users online

--- Framework/Session.php
+++ Framework/Session.php
@@ -56,4 +56,52 @@ class Session
     self::clear('flash_' . $key);
     return $message;
   }
+
+  public static function sessionsCount(){
+    $session_path = session_save_path();
+
+    $handle = opendir($session_path);
+    $sessions = 0;
+        while (($file = readdir($handle)) != FALSE) {
+           
+            if(in_array($file, ['.', '..']))
+                continue;
+            if(!preg_match("/^sess/", $file))
+                continue;
+            $sessions++;
+        }
+        return $sessions;
+  }
+
+
+  public static function sessions5Minutes(){
+    
+    $session_path = session_save_path();
+    $handle = opendir($session_path);
+    $sessions = 0;
+    clearstatcache();
+    static::set("_cache", (string)time());
+
+        while (($file = readdir($handle)) != FALSE) {
+           
+            if(in_array($file, ['.', '..']))
+                continue;
+            if(!preg_match("/^sess/", $file))
+                continue;
+           
+            $file_path = "{$session_path}/{$file}";
+            $last_access = date("Y-m-d H:i:s", fileatime($file_path));
+            $now = date("Y-m-d H:i:s", strtotime("-5 minutes"));
+            
+            if($now <= $last_access)
+                $sessions++;
+            
+        }
+    return $sessions;
+}
+
+public static function onEveryRequestMiddleware(){
+    static::start();
+    static::set("_cache", (string)time());
+}
 }
